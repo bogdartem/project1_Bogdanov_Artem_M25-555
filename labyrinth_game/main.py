832e6e44f21ba7510fcaa743cf69260c9b1d54eb
Богданов Артём <bogdartem1997@yandex.ru>
2025-10-09 22:22:26 +0300

    Architecture and data formation

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
+import labyrinth_game.constants
+import labyrinth_game.player_actions
+import labyrinth_game.utils
+
+
 def main():
     print("Первая попытка запустить проект!")
+    game_state = {
+        'player_inventory': [],  # Инвентарь игрока
+        'current_room': 'entrance',  # Текущая комната
+        'game_over': False,  # Значения окончания игры
+        'steps_taken': 0  # Количество шагов
+    }
 
 
 if __name__ == "__main__":
