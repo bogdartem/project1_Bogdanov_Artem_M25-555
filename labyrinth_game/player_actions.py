5d16048b171dbc15ca1dc754f7004cf4c22055c8
Богданов Артём <bogdartem1997@yandex.ru>
2025-10-16 17:20:24 +0300

    Redact player_actions: add func get_input

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -3,3 +3,10 @@ def show_inventory(game_state):
         print('Инвентарь пуст.')
     elif game_state['player_inventory']:
         print(f'Инвентарь игрока: {', '.join(game_state['player_inventory'])}')
+
+def get_input(prompt="> "):
+    try:
+        pass
+    except (KeyboardInterrupt, EOFError):
+        print("\nВыход из игры.")
+        return "quit" 
