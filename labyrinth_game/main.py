e4cf6545d31f8143d4af99da90a4e1e58f548bcc
Богданов Артём <bogdartem1997@yandex.ru>
2025-10-16 17:52:14 +0300

    Redact player_actions: add func move_player

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from labyrinth_game.player_actions import show_inventory
+from labyrinth_game.player_actions import move_player, show_inventory
 from labyrinth_game.utils import describe_current_room
 # import labyrinth_game.constants
 
@@ -15,6 +15,7 @@ def main():
         'steps_taken': 0  # Количество шагов
     }
     describe_current_room(game_state)
+    # move_player(game_state, 'north')
     # show_inventory(game_state)
     # Создайте цикл while, который будет работать, пока игра не окончена.
     # Внутри цикла считывайте команду от пользователя.
--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,12 +1,30 @@
+from labyrinth_game.constants import ROOMS
+from labyrinth_game.utils import describe_current_room
+
+
 def show_inventory(game_state):
     if not game_state['player_inventory']:
         print('Инвентарь пуст.')
     elif game_state['player_inventory']:
         print(f'Инвентарь игрока: {', '.join(game_state['player_inventory'])}')
 
-def get_input(prompt="> "):
+
+def get_input(prompt='> '):
     try:
-        pass
+        pass  # дописать
     except (KeyboardInterrupt, EOFError):
-        print("\nВыход из игры.")
-        return "quit" 
+        print('Выход из игры.')
+        return 'quit' 
+
+
+def move_player(game_state, direction: str):
+    current_room = game_state['current_room']
+    room_data = ROOMS[current_room]
+    if direction in room_data['exits'].keys():
+        print('\nВыход в другую комнату есть\n')
+        game_state['current_room'] = room_data['exits'][direction]  # Обновите текущую комнату.
+        game_state['steps_taken'] += 1  # Увеличьте шаг на единицу.
+        describe_current_room(game_state)  # Выведите описание новой комнаты.
+    elif direction not in room_data['exits'].keys():
+        print('Нельзя пойти в этом направлении.')
+    pass  # дописать
